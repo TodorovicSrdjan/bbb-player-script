cb6031ad5d58511f930e6b9ee5a27c0bbed1174b
TodorovicSrdjan <tsrdjan@pm.me>
2021-05-16 02:11:05 +0200

    Update first_run.sh
    - podeseno automatsko skidanje bbb-playera
    - dodate odgovarajuce naredbe za slucajeve kada komande nisu uspesno izvrsene
    - sakriven folder bbb-player

--- first_run.sh
+++ first_run.sh
@@ -1,16 +1,20 @@
-#!/bin/bash -li
+#!/bin/bash
 
 putanja=$(echo "$(cd "$(dirname "$0")" && pwd )")
 
-python3 -m venv "$putanja"/bbb-player/env && source "$putanja"/bbb-player/env/bin/activate
+url="https://github.com/andrazznidar/bbb-player.git"
 
-echo -e 'Virtualnog okruzenja je kreirano\n'
+git clone $url && mv bbb-player .bbb-player || echo "Problem sa git-om ili komandom mv"; sudo apt-get install git; git clone $url && mv bbb-player .bbb-player|| exit 1
 
-pip install -r "$putanja"/bbb-player/requirements.txt
+python3 -m venv "$putanja"/.bbb-player/env && source "$putanja"/.bbb-player/env/bin/activate || echo -e "\"venv\" nije instaliran"; sudo apt-get install python3-venv; python3 -m venv "$putanja"/.bbb-player/env && source "$putanja"/.bbb-player/env/bin/activate || exit 2
+
+echo -e '\nVirtualnog okruzenja je kreirano\n'
+
+pip install -r "$putanja"/.bbb-player/requirements.txt
 
 echo -e '\nSvi potrebni programi su instalirani\n'
 
-source "$putanja"/bbb-player/env/bin/activate
+source "$putanja"/.bbb-player/env/bin/activate
 
 echo 'Napravi stalni alias za skriptu (y|n)? '
 read
@@ -22,9 +26,9 @@ if [[ "$REPLY" = 'y' || "$REPLY" = 'Y' ]]; then
     source ~/.bash_aliases
 fi
 
-mkdir "$putanja"/bbb-player/downloadedMeetings 2>/dev/null || echo -e "Direktorijum \"downloadedMeetings\" je vec kreiran\n"
+mkdir "$putanja"/.bbb-player/downloadedMeetings 2>/dev/null || echo -e "Direktorijum \"downloadedMeetings\" je vec kreiran\n"
 
-python "$putanja"/bbb-player/bbb-player.py -s --no-check-certificate &
+python "$putanja"/.bbb-player/bbb-player.py -s --no-check-certificate &
 
 pid1=$!
 
@@ -34,7 +38,9 @@ echo -e "import webbrowser\\nwebbrowser.open_new_tab(\"http://localhost:5000/\")
 
 pid2=$!
 
-wait $pid1 $pid2
+wait $pid2
+
+kill -9 $pid1
 
 deactivate
 
